20175aaa0a5191a6b4c62fbbe7b817dbfed0799b
Jhan <yank0622@gmail.com>
2023-11-09 20:15:44 -0500

    prepare app for deploy in vercel

--- api/index.js
+++ api/index.js
@@ -4,30 +4,33 @@ const cors = require("cors");
 const { errorHandler, errorLog, boomHander } = require("./middlewares/errorHandler");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const whiteList = ['https://localhost:8080.', 'https://myapp.co'];
+const whiteList = ['https://localhost:8080', 'https://myapp.com'];
 
 const coreOpts = {
-  origin: (origin, callback) =>{
-    if (whiteList.includes(origin)) {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
+    } else {
+      callback(new Error("don't allowed"));
     }
-    callback(new Error("don't allowed"));
+
   }
 };
 app.use(cors(coreOpts));
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mundo');
 });
 
 routerApi(app);
+
 app.use(errorLog);
 app.use(boomHander);
 app.use(errorHandler);
